0f473f20ff88a0b607ef4296f367a2e6530ce177
Abhi-atg-1021 <abhisinghal112@gmail.com>
2023-06-11 16:10:15 +0530

    changes

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use(cookieParser())
 require('./config/db')()
 const port = process.env.PORT||5000
 
-// app.use(function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//     res.header(
-//       'Access-Control-Allow-Headers',
-//       'Origin, X-Requested-With, Content-Type, Accept'
-//     );
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://funny-halva-a4a187.netlify.app/');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
 
 app.use(express.json());
 
