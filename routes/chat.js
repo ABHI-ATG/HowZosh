225a8cb09b0a9723caa280d680ee0349b0121890
Abhi-atg-1021 <abhisinghal112@gmail.com>
2023-06-11 17:15:35 +0530

    changes

--- routes/chat.js
+++ routes/chat.js
@@ -2,7 +2,7 @@ const app=require('express')();
 const {search,create,send,dashboard,deleteNotification}=require('../contoller/chatController')
 const authenticate=require('../middleware/authenticate')
 
-app.get('/dashboard',authenticate,dashboard);
+app.get('/dashboard',dashboard);
 app.get('/search',authenticate,search);
 app.post('/create',authenticate,create);
 app.post('/send',authenticate,send);
